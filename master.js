eb52c314ac17c76e426593b4c01f7d0527b04cb8
Daniel X Moore <yahivin@gmail.com>
2013-09-29 19:45:11 -0700

    :ocean::sparkles: Updated in browser at strd6.github.io/editor

--- master.js
+++ master.js
@@ -1,4 +1,12 @@
 (function() {
+  var runtime;
+
   require("hotkeys");
+
+  runtime = require("runtime")(PACKAGE);
+
+  runtime.boot();
+
+  runtime.applyStyleSheet(require('./style'));
 
 }).call(this);
