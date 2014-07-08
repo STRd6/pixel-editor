ca8a22512881d77e5598410f70dd317b10c0619f
Daniel X Moore <yahivin@gmail.com>
2014-07-07 17:16:08 -0700

    :sunny::cherry_blossom: Updated in browser at strd6.github.io/editor

--- master.json.js
+++ master.json.js
@@ -128,7 +128,7 @@ window["STRd6/pixel-editor:master"]({
     },
     "main.coffee.md": {
       "path": "main.coffee.md",
-      "content": "Pixel Editor\n============\n\nWelcome to this cool pixel editor. Eventually you'll be able to read this for\nhelp, but right now it's mostly code.\n\nEditing pixels in your browser.\n\n    # For debug purposes\n    global.PACKAGE = PACKAGE\n    global.require = require\n\n    # Google Analytics\n    require(\"analytics\").init(\"UA-3464282-15\")\n\n    # Setup\n    global.Observable = require \"observable\"\n    global.Model = require \"model\"\n    global.Bindable = require \"bindable\"\n    require \"cornerstone\"\n\n    require \"appcache\"\n    require \"jquery-utils\"\n\n    require \"./lib/canvas-to-blob\"\n\n    runtime = require(\"runtime\")(PACKAGE)\n    runtime.boot()\n    runtime.applyStyleSheet(require('./style'))\n\n    Editor = require \"./editor\"\n\n    # For debugging\n    global.editor = Editor()\n\n    editor.notify(\"Welcome to PixiPaint!\")\n",
+      "content": "Pixel Editor\n============\n\nWelcome to this cool pixel editor. Eventually you'll be able to read this for\nhelp, but right now it's mostly code.\n\nEditing pixels in your browser.\n\n    # For debug purposes\n    global.PACKAGE = PACKAGE\n    global.require = require\n\n    # Google Analytics\n    require(\"analytics\").init(\"UA-3464282-15\")\n\n    # Setup\n    global.Observable = require \"observable\"\n    global.Model = require \"model\"\n    global.Bindable = require \"bindable\"\n    require \"cornerstone\"\n\n    require \"jquery-utils\"\n\n    require \"./lib/canvas-to-blob\"\n\n    runtime = require(\"runtime\")(PACKAGE)\n    runtime.boot()\n    runtime.applyStyleSheet(require('./style'))\n\n    Editor = require \"./editor\"\n\n    # For debugging\n    global.editor = Editor()\n\n    editor.notify(\"Welcome to PixiPaint!\")\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -152,7 +152,7 @@ window["STRd6/pixel-editor:master"]({
     },
     "pixie.cson": {
       "path": "pixie.cson",
-      "content": "version: \"0.1.0\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  analytics: \"distri/google-analytics:v0.1.0\"\n  appcache: \"distri/appcache:v0.2.0\"\n  bindable: \"distri/bindable:v0.1.0\"\n  byte_array: \"distri/byte_array:v0.1.1\"\n  cornerstone: \"distri/cornerstone:v0.2.2-pre.0\"\n  model: \"distri/model:v0.1.2\"\n  eval: \"distri/eval:v0.1.0\"\n  facebook: \"distri/facebook:v0.1.1\"\n  \"grid-gen\": \"distri/grid-gen:v0.2.0\"\n  \"hotkeys\": \"distri/hotkeys:v0.2.0\"\n  \"jquery-utils\": \"distri/jquery-utils:v0.2.0\"\n  observable: \"distri/observable:v0.2.0-pre.1\"\n  postmaster: \"distri/postmaster:v0.2.0\"\n  runtime: \"distri/runtime:v0.3.0\"\n  \"touch-canvas\": \"distri/touch-canvas:v0.3.1\"\n  \"undo\": \"distri/undo:v0.2.0\"\n  util: \"distri/util:v0.1.0\"\nwidth: 1280\nheight: 720\n",
+      "content": "version: \"0.1.0\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  analytics: \"distri/google-analytics:v0.1.0\"\n  bindable: \"distri/bindable:v0.1.0\"\n  byte_array: \"distri/byte_array:v0.1.1\"\n  cornerstone: \"distri/cornerstone:v0.2.2-pre.0\"\n  model: \"distri/model:v0.1.2\"\n  eval: \"distri/eval:v0.1.0\"\n  facebook: \"distri/facebook:v0.1.1\"\n  \"grid-gen\": \"distri/grid-gen:v0.2.0\"\n  \"hotkeys\": \"distri/hotkeys:v0.2.0\"\n  \"jquery-utils\": \"distri/jquery-utils:v0.2.0\"\n  observable: \"distri/observable:v0.2.0-pre.1\"\n  postmaster: \"distri/postmaster:v0.2.0\"\n  runtime: \"distri/runtime:v0.3.0\"\n  \"touch-canvas\": \"distri/touch-canvas:v0.3.1\"\n  \"undo\": \"distri/undo:v0.2.0\"\n  util: \"distri/util:v0.1.0\"\nwidth: 1280\nheight: 720\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -323,7 +323,7 @@ window["STRd6/pixel-editor:master"]({
     },
     "main": {
       "path": "main",
-      "content": "(function() {\n  var Editor, runtime;\n\n  global.PACKAGE = PACKAGE;\n\n  global.require = require;\n\n  require(\"analytics\").init(\"UA-3464282-15\");\n\n  global.Observable = require(\"observable\");\n\n  global.Model = require(\"model\");\n\n  global.Bindable = require(\"bindable\");\n\n  require(\"cornerstone\");\n\n  require(\"appcache\");\n\n  require(\"jquery-utils\");\n\n  require(\"./lib/canvas-to-blob\");\n\n  runtime = require(\"runtime\")(PACKAGE);\n\n  runtime.boot();\n\n  runtime.applyStyleSheet(require('./style'));\n\n  Editor = require(\"./editor\");\n\n  global.editor = Editor();\n\n  editor.notify(\"Welcome to PixiPaint!\");\n\n}).call(this);\n",
+      "content": "(function() {\n  var Editor, runtime;\n\n  global.PACKAGE = PACKAGE;\n\n  global.require = require;\n\n  require(\"analytics\").init(\"UA-3464282-15\");\n\n  global.Observable = require(\"observable\");\n\n  global.Model = require(\"model\");\n\n  global.Bindable = require(\"bindable\");\n\n  require(\"cornerstone\");\n\n  require(\"jquery-utils\");\n\n  require(\"./lib/canvas-to-blob\");\n\n  runtime = require(\"runtime\")(PACKAGE);\n\n  runtime.boot();\n\n  runtime.applyStyleSheet(require('./style'));\n\n  Editor = require(\"./editor\");\n\n  global.editor = Editor();\n\n  editor.notify(\"Welcome to PixiPaint!\");\n\n}).call(this);\n",
       "type": "blob"
     },
     "modal": {
@@ -343,7 +343,7 @@ window["STRd6/pixel-editor:master"]({
     },
     "pixie": {
       "path": "pixie",
-      "content": "module.exports = {\"version\":\"0.1.0\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"analytics\":\"distri/google-analytics:v0.1.0\",\"appcache\":\"distri/appcache:v0.2.0\",\"bindable\":\"distri/bindable:v0.1.0\",\"byte_array\":\"distri/byte_array:v0.1.1\",\"cornerstone\":\"distri/cornerstone:v0.2.2-pre.0\",\"model\":\"distri/model:v0.1.2\",\"eval\":\"distri/eval:v0.1.0\",\"facebook\":\"distri/facebook:v0.1.1\",\"grid-gen\":\"distri/grid-gen:v0.2.0\",\"hotkeys\":\"distri/hotkeys:v0.2.0\",\"jquery-utils\":\"distri/jquery-utils:v0.2.0\",\"observable\":\"distri/observable:v0.2.0-pre.1\",\"postmaster\":\"distri/postmaster:v0.2.0\",\"runtime\":\"distri/runtime:v0.3.0\",\"touch-canvas\":\"distri/touch-canvas:v0.3.1\",\"undo\":\"distri/undo:v0.2.0\",\"util\":\"distri/util:v0.1.0\"},\"width\":1280,\"height\":720};",
+      "content": "module.exports = {\"version\":\"0.1.0\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"analytics\":\"distri/google-analytics:v0.1.0\",\"bindable\":\"distri/bindable:v0.1.0\",\"byte_array\":\"distri/byte_array:v0.1.1\",\"cornerstone\":\"distri/cornerstone:v0.2.2-pre.0\",\"model\":\"distri/model:v0.1.2\",\"eval\":\"distri/eval:v0.1.0\",\"facebook\":\"distri/facebook:v0.1.1\",\"grid-gen\":\"distri/grid-gen:v0.2.0\",\"hotkeys\":\"distri/hotkeys:v0.2.0\",\"jquery-utils\":\"distri/jquery-utils:v0.2.0\",\"observable\":\"distri/observable:v0.2.0-pre.1\",\"postmaster\":\"distri/postmaster:v0.2.0\",\"runtime\":\"distri/runtime:v0.3.0\",\"touch-canvas\":\"distri/touch-canvas:v0.3.1\",\"undo\":\"distri/undo:v0.2.0\",\"util\":\"distri/util:v0.1.0\"},\"width\":1280,\"height\":720};",
       "type": "blob"
     },
     "stripe-payment": {
@@ -611,167 +611,6 @@ window["STRd6/pixel-editor:master"]({
         "subscribers_count": 2,
         "branch": "v0.1.0",
         "publishBranch": "gh-pages"
-      },
-      "dependencies": {}
-    },
-    "appcache": {
-      "source": {
-        "LICENSE": {
-          "path": "LICENSE",
-          "mode": "100644",
-          "content": "The MIT License (MIT)\n\nCopyright (c) 2013 Daniel X Moore\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of\nthis software and associated documentation files (the \"Software\"), to deal in\nthe Software without restriction, including without limitation the rights to\nuse, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\nthe Software, and to permit persons to whom the Software is furnished to do so,\nsubject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\nFOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\nCOPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\nIN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\nCONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n",
-          "type": "blob"
-        },
-        "README.md": {
-          "path": "README.md",
-          "mode": "100644",
-          "content": "appcache\n========\n\nHTML5 AppCache Helpers\n",
-          "type": "blob"
-        },
-        "main.coffee.md": {
-          "path": "main.coffee.md",
-          "mode": "100644",
-          "content": "App Cache\n=========\n\nSome helpers for working with HTML5 application cache.\n\nhttp://www.html5rocks.com/en/tutorials/appcache/beginner/\n\n    applicationCache = window.applicationCache\n\n    applicationCache.addEventListener 'updateready', (e) ->\n      if applicationCache.status is applicationCache.UPDATEREADY\n        # Browser downloaded a new app cache.\n        if confirm('A new version of this site is available. Load it?')\n          window.location.reload()\n    , false\n",
-          "type": "blob"
-        },
-        "pixie.cson": {
-          "path": "pixie.cson",
-          "mode": "100644",
-          "content": "version: \"0.2.0\"\nentryPoint: \"main\"\n",
-          "type": "blob"
-        }
-      },
-      "distribution": {
-        "main": {
-          "path": "main",
-          "content": "(function() {\n  var applicationCache;\n\n  applicationCache = window.applicationCache;\n\n  applicationCache.addEventListener('updateready', function(e) {\n    if (applicationCache.status === applicationCache.UPDATEREADY) {\n      if (confirm('A new version of this site is available. Load it?')) {\n        return window.location.reload();\n      }\n    }\n  }, false);\n\n}).call(this);\n\n//# sourceURL=main.coffee",
-          "type": "blob"
-        },
-        "pixie": {
-          "path": "pixie",
-          "content": "module.exports = {\"version\":\"0.2.0\",\"entryPoint\":\"main\"};",
-          "type": "blob"
-        }
-      },
-      "progenitor": {
-        "url": "http://strd6.github.io/editor/"
-      },
-      "version": "0.2.0",
-      "entryPoint": "main",
-      "repository": {
-        "id": 14539483,
-        "name": "appcache",
-        "full_name": "distri/appcache",
-        "owner": {
-          "login": "distri",
-          "id": 6005125,
-          "avatar_url": "https://identicons.github.com/f90c81ffc1498e260c820082f2e7ca5f.png",
-          "gravatar_id": null,
-          "url": "https://api.github.com/users/distri",
-          "html_url": "https://github.com/distri",
-          "followers_url": "https://api.github.com/users/distri/followers",
-          "following_url": "https://api.github.com/users/distri/following{/other_user}",
-          "gists_url": "https://api.github.com/users/distri/gists{/gist_id}",
-          "starred_url": "https://api.github.com/users/distri/starred{/owner}{/repo}",
-          "subscriptions_url": "https://api.github.com/users/distri/subscriptions",
-          "organizations_url": "https://api.github.com/users/distri/orgs",
-          "repos_url": "https://api.github.com/users/distri/repos",
-          "events_url": "https://api.github.com/users/distri/events{/privacy}",
-          "received_events_url": "https://api.github.com/users/distri/received_events",
-          "type": "Organization",
-          "site_admin": false
-        },
-        "private": false,
-        "html_url": "https://github.com/distri/appcache",
-        "description": "HTML5 AppCache Helpers",
-        "fork": false,
-        "url": "https://api.github.com/repos/distri/appcache",
-        "forks_url": "https://api.github.com/repos/distri/appcache/forks",
-        "keys_url": "https://api.github.com/repos/distri/appcache/keys{/key_id}",
-        "collaborators_url": "https://api.github.com/repos/distri/appcache/collaborators{/collaborator}",
-        "teams_url": "https://api.github.com/repos/distri/appcache/teams",
-        "hooks_url": "https://api.github.com/repos/distri/appcache/hooks",
-        "issue_events_url": "https://api.github.com/repos/distri/appcache/issues/events{/number}",
-        "events_url": "https://api.github.com/repos/distri/appcache/events",
-        "assignees_url": "https://api.github.com/repos/distri/appcache/assignees{/user}",
-        "branches_url": "https://api.github.com/repos/distri/appcache/branches{/branch}",
-        "tags_url": "https://api.github.com/repos/distri/appcache/tags",
-        "blobs_url": "https://api.github.com/repos/distri/appcache/git/blobs{/sha}",
-        "git_tags_url": "https://api.github.com/repos/distri/appcache/git/tags{/sha}",
-        "git_refs_url": "https://api.github.com/repos/distri/appcache/git/refs{/sha}",
-        "trees_url": "https://api.github.com/repos/distri/appcache/git/trees{/sha}",
-        "statuses_url": "https://api.github.com/repos/distri/appcache/statuses/{sha}",
-        "languages_url": "https://api.github.com/repos/distri/appcache/languages",
-        "stargazers_url": "https://api.github.com/repos/distri/appcache/stargazers",
-        "contributors_url": "https://api.github.com/repos/distri/appcache/contributors",
-        "subscribers_url": "https://api.github.com/repos/distri/appcache/subscribers",
-        "subscription_url": "https://api.github.com/repos/distri/appcache/subscription",
-        "commits_url": "https://api.github.com/repos/distri/appcache/commits{/sha}",
-        "git_commits_url": "https://api.github.com/repos/distri/appcache/git/commits{/sha}",
-        "comments_url": "https://api.github.com/repos/distri/appcache/comments{/number}",
-        "issue_comment_url": "https://api.github.com/repos/distri/appcache/issues/comments/{number}",
-        "contents_url": "https://api.github.com/repos/distri/appcache/contents/{+path}",
-        "compare_url": "https://api.github.com/repos/distri/appcache/compare/{base}...{head}",
-        "merges_url": "https://api.github.com/repos/distri/appcache/merges",
-        "archive_url": "https://api.github.com/repos/distri/appcache/{archive_format}{/ref}",
-        "downloads_url": "https://api.github.com/repos/distri/appcache/downloads",
-        "issues_url": "https://api.github.com/repos/distri/appcache/issues{/number}",
-        "pulls_url": "https://api.github.com/repos/distri/appcache/pulls{/number}",
-        "milestones_url": "https://api.github.com/repos/distri/appcache/milestones{/number}",
-        "notifications_url": "https://api.github.com/repos/distri/appcache/notifications{?since,all,participating}",
-        "labels_url": "https://api.github.com/repos/distri/appcache/labels{/name}",
-        "releases_url": "https://api.github.com/repos/distri/appcache/releases{/id}",
-        "created_at": "2013-11-19T22:09:16Z",
-        "updated_at": "2013-11-29T20:49:51Z",
-        "pushed_at": "2013-11-19T22:10:28Z",
-        "git_url": "git://github.com/distri/appcache.git",
-        "ssh_url": "git@github.com:distri/appcache.git",
-        "clone_url": "https://github.com/distri/appcache.git",
-        "svn_url": "https://github.com/distri/appcache",
-        "homepage": null,
-        "size": 240,
-        "stargazers_count": 0,
-        "watchers_count": 0,
-        "language": "CoffeeScript",
-        "has_issues": true,
-        "has_downloads": true,
-        "has_wiki": true,
-        "forks_count": 0,
-        "mirror_url": null,
-        "open_issues_count": 0,
-        "forks": 0,
-        "open_issues": 0,
-        "watchers": 0,
-        "default_branch": "master",
-        "master_branch": "master",
-        "permissions": {
-          "admin": true,
-          "push": true,
-          "pull": true
-        },
-        "organization": {
-          "login": "distri",
-          "id": 6005125,
-          "avatar_url": "https://identicons.github.com/f90c81ffc1498e260c820082f2e7ca5f.png",
-          "gravatar_id": null,
-          "url": "https://api.github.com/users/distri",
-          "html_url": "https://github.com/distri",
-          "followers_url": "https://api.github.com/users/distri/followers",
-          "following_url": "https://api.github.com/users/distri/following{/other_user}",
-          "gists_url": "https://api.github.com/users/distri/gists{/gist_id}",
-          "starred_url": "https://api.github.com/users/distri/starred{/owner}{/repo}",
-          "subscriptions_url": "https://api.github.com/users/distri/subscriptions",
-          "organizations_url": "https://api.github.com/users/distri/orgs",
-          "repos_url": "https://api.github.com/users/distri/repos",
-          "events_url": "https://api.github.com/users/distri/events{/privacy}",
-          "received_events_url": "https://api.github.com/users/distri/received_events",
-          "type": "Organization",
-          "site_admin": false
-        },
-        "network_count": 0,
-        "subscribers_count": 1,
-        "branch": "v0.2.0",
-        "defaultBranch": "master"
       },
       "dependencies": {}
     },
